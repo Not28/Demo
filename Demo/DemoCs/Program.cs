7e802b91d1eddc8261f122131b6326b6ee719432
Not28 <117514195+Not28@users.noreply.github.com>
2022-12-14 13:54:34 +0700

    hoàn tất dấu hỏi

--- Demo/DemoCs/Program.cs
+++ Demo/DemoCs/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// format code ctrl + k + d
+double a = 0.1;
+double b = 0.2;
+Console.WriteLine(a + b == 0.3 ? "true" : "false");
+
+decimal c = 0.1M;
+decimal d = 0.2M;
+Console.WriteLine(c + d == 0.3M);
+
+//?: => ternary operator
+
+string fullName = null;
+//int i = null;
+//c# cũ
+//Nullable<int> i = null;
+//c# mới
+bool? check = null;
+
+//int length = fullName != null ? fullName.length : 0;
+//int? length = fullName is not null ? fullName.Length : null;
+
+//null-conditional operator
+// ? tương tự != , fullName mà khác null thì trả về vế sau.
+int? length = fullName?.Length;
+
+// null-coalescing operator ??
+// tương tự dấu ==, fullName bằng null thì trả về vế sau
+int? chieudai = fullName?.Length ?? 0;
