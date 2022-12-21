c70bab0efde31f4278ce470164c743eef225fcb5
Not28 <117514195+Not28@users.noreply.github.com>
2022-12-21 13:58:37 +0700

    ListAndLinQ

---
+++ Demo/ListAndLinQ/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+List<Student> list = new()
+{
+    new Student() {RollNumber= 1 , FullName="Not", Section="dãy nhà số 1" , HosterNumber = 1},
+    new Student() {RollNumber= 2 , FullName="Not1", Section="dãy nhà số 2" , HosterNumber = 2},
+    new Student() {RollNumber= 3 , FullName="Not2", Section="dãy nhà số 3" , HosterNumber = 5},
+    new Student() {RollNumber= 4 , FullName="Not3", Section="dãy nhà số 4" , HosterNumber = 7},
+    new Student() {RollNumber= 1 , FullName="Gin", Section="dãy nhà số 1" , HosterNumber = 3},
+    new Student() {RollNumber= 2 , FullName="Gin2", Section="dãy nhà số 2" , HosterNumber = 5},
+    new Student() {RollNumber= 3 , FullName="Gin3", Section="dãy nhà số 3" , HosterNumber = 4},
+    new Student() {RollNumber= 4 , FullName="Gin4", Section="dãy nhà số 4" , HosterNumber = 6},
+};
+
+Stopwatch sw= new Stopwatch();
+sw.Restart();
+foreach (var stu in list )
+{
+    Console.WriteLine(stu);
+}
+Console.WriteLine($"foreach time: {sw.ElapsedMilliseconds}");
+sw.Restart();
+
+//tất cả các dạng của Colection đều có mẫu để duyệt qua dữ liệu
+//mà không dùng for hay foreach => ienumerator
+
+
+//chạy nhanh hơn
+IEnumerator enu = list.GetEnumerator();
+//IEnumerator <Student> = list.GetEnumerator();
+while (enu.MoveNext())
+{
+    Console.WriteLine(enu.Current);
+}
+Console.WriteLine($"foreach time: {sw.ElapsedMilliseconds}");
+sw.Restart();
+
+//linq => language Intergrated query
+
+//linq to object
+//linq to sql
+//linq to xml,json
+//parallel linq => plinq (đi làm )
+
+//select from where group by having order
+//from where group by having order => select
+
+
+//linq to object có 2 dạng
+//style 1) theo trường phái sql=> query syntax , dễ học, dễ hiểu
+//style 2) theo trường phái lamda=>method syntax, khó học , rất khó hiểu
---
+++ Demo/ListAndLinQ/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace ListAndLinQ;
+internal class Student
+{
+    public int RollNumber { get; set; }
+    public string FullName { get; set; }
+    public string Section { get; set; }
+    public int    HosterNumber{ get; set; }
+
+    public override string ToString()
+    {
+        return $"{{{nameof(RollNumber)}={RollNumber}, {nameof(FullName)}={FullName}, {nameof(Section)}={Section}, {nameof(HosterNumber)}={HosterNumber.ToString()}}}";
+    }
+}
