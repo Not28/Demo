56240c5ca1fb1a37e6618607bb5d78ba4d320ed2
Not28 <117514195+Not28@users.noreply.github.com>
2022-12-21 13:12:42 +0700

    ExtentionMethod

---
+++ Demo/ExlotionMethod/ExtentionCheck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethod;
+//biến class này thành Extention Method
+//=>Class buộc phải là Static
+//1 Class là static thì toàn bộ buộc phải là Static
+internal static class ExtentionCheck
+{
+    public static void IsGetterThan(int i , int value)
+    {
+        Console.WriteLine($"{i}đúng là lớn hơn{value}");
+    }
+
+    public static void Hi(this Program obj) => Console.WriteLine("Loo");
+    
+}
---
+++ Demo/ExlotionMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using ExtentionMethod;
+using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+Console.WriteLine("vui lòng nhập 1 số");
+int i = int.TryParse(Console.ReadLine(), out var result)?result:0;
+
+// Làm kiểm tra số lớn hơn 100
+CheckNumber.Check(i, 100);
+
+
+
+i.IsGetterThan(100);
+int abc = 500;
+abc.IsGetterThan(100);
+
+//Extention Method không thông qua đối tượng của class đang chứa
+//mà thông qua cái muốn sử dụng
+
+Program pro = new();
+pro.Hi();
