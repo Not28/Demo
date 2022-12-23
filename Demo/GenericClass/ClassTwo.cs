0495ca851954f7e5bacf92d7ef3942ca4193f100
Not28 <117514195+Not28@users.noreply.github.com>
2022-12-23 14:39:29 +0700

    GenericClass

---
+++ Demo/GenericClass/ClassTwo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass;
+internal class ClassTwo<A,B>
+{
+    public A Field1 { get; set; }
+    public B Field2 { get; set; }
+
+    public void Show() => Console.WriteLine($"{ Field1}, {Field2}"); 
+
+}
---
+++ Demo/GenericClass/Program.cs
@@ -0,0 +1,10 @@
+﻿using GenericClass;
+
+ClassOne<int> one = new ClassOne<int>();
+one.Field = 10;
+one.Show();
+
+ClassTwo<string , int> two= new ClassTwo<string , int>();
+two.Field1 = "not";
+two.Field2 = 10;
+two.Show();
