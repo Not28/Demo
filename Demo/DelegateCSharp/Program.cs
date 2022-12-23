46a698e755e03fba01f196666255a4435bab4d67
Not28 <117514195+Not28@users.noreply.github.com>
2022-12-23 15:01:47 +0700

    Delegate

---
+++ Demo/DelegateCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using DelegateCSharp;
+//c1
+//Mathems ma = new();
+//ma.Add(10, 5);
+////c2
+//Mathems.Div(10, 6);
+////c3
+//new Mathems().Multi(10, 5);
+
+//delegate
+Abc dele = new Mathems().Multi;
+dele(10, 5);
